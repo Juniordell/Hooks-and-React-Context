a58d6d7e25361696fe8c6fb65c160674fe185e1e
Nelson Jr <juniordell123@gmail.com>
2020-07-20 10:01:23 -0300

    Add navbar, book list and css

---
+++ src/components/BookList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+function BookList() {
+  return (
+      <div className='book-list'>
+          <ul>
+            <li>the way of kings</li>
+            <li>the name of the wind</li>
+            <li>the final empire</li>
+          </ul>
+      </div>
+  );
+}
+
+export default BookList;
---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+function Navbar() {
+  return (
+      <nav>
+          <h1>Context App</h1>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+      </nav>
+  );
+}
+
+export default Navbar;
